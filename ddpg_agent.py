8b8f5e272f548cbdf0514799bbe3d62ec65472b5
KhalilGorsan <khalilgorsan@gmail.com>
2020-07-08 20:24:02 +0100

    fix: change configs

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -13,8 +13,8 @@ BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 128  # minibatch size
 GAMMA = 0.99  # discount factor
 TAU = 1e-3  # for soft update of target parameters
-LR_ACTOR = 1e-4  # learning rate of the actor
-LR_CRITIC = 1e-3  # learning rate of the critic
+LR_ACTOR = 2e-4  # learning rate of the actor
+LR_CRITIC = 2e-4  # learning rate of the critic
 WEIGHT_DECAY = 0  # L2 weight decay
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -86,7 +86,7 @@ class Agent:
             critic_target(state, action) -> Q-value
         Params
         ======
-            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples 
+            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples
             gamma (float): discount factor
         """
         states, actions, rewards, next_states, dones = experiences
--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from core import ReacherWrapper
 from ddpg_agent import Agent
 
 
-def ddpg(env, agent, n_episodes=1000, max_t=1000, print_every=100):
+def ddpg(env, agent, n_episodes=500, max_t=1000, print_every=100):
     scores_deque = deque(maxlen=print_every)
     scores = []
     for i_episode in range(1, n_episodes + 1):
@@ -42,7 +42,7 @@ def ddpg(env, agent, n_episodes=1000, max_t=1000, print_every=100):
         if np.mean(scores_deque) >= 30.0:
             print(
                 "\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}".format(
-                    i_episode - 100, np.mean(scores_deque)
+                    i_episode, np.mean(scores_deque)
                 )
             )
             torch.save(agent.actor_local.state_dict(), "checkpoint_actor.pth")
@@ -55,7 +55,7 @@ def main():
     env = ReacherWrapper(file_name="./Reacher")
     state_size = env.observation_size
     action_size = env.action_size
-    agent = Agent(state_size=state_size, action_size=action_size, random_seed=0)
+    agent = Agent(state_size=state_size, action_size=action_size, random_seed=10)
 
     scores = ddpg(env, agent)
 
