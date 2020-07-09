92c00c322836a43bea7ea9bf17950b9607f3ef6c
KhalilGorsan <khalilgorsan@gmail.com>
2020-07-10 00:28:36 +0100

    learning: Tune the Ornstein-Uhlenbeck noise

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -147,7 +147,7 @@ class Agent:
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
 
-    def __init__(self, size, seed, mu=0.0, theta=0.15, sigma=0.2):
+    def __init__(self, size, seed, mu=0.0, theta=0.15, sigma=0.1):
         """Initialize parameters and noise process."""
         self.mu = mu * np.ones(size)
         self.theta = theta
