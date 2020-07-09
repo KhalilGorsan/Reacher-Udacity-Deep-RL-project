d912e39c2c08c90ea929bf153ecfa1f8fe7d0d8d
KhalilGorsan <khalilgorsan@gmail.com>
2020-07-09 02:09:37 +0100

    feat: Add batch normalization to actor and critic networks

--- model.py
+++ model.py
@@ -26,6 +26,8 @@ class Actor(nn.Module):
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.fc1 = nn.Linear(state_size, fc1_units)
+        # applying a Batch Normalization on the first layer output
+        self.bn1 = nn.BatchNorm1d(fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
         self.reset_parameters()
@@ -37,7 +39,11 @@ class Actor(nn.Module):
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
+        if state.dim() == 1:
+            state = torch.unsqueeze(state, 0)
         x = F.relu(self.fc1(state))
+        # applying a batch Normalization on the first layer output
+        x = self.bn1(x)
         x = F.relu(self.fc2(x))
         return F.tanh(self.fc3(x))
 
@@ -58,6 +64,8 @@ class Critic(nn.Module):
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.fcs1 = nn.Linear(state_size, fcs1_units)
+        # applying a batch Normalization on the first layer output
+        self.bn1 = nn.BatchNorm1d(fcs1_units)
         self.fc2 = nn.Linear(fcs1_units + action_size, fc2_units)
         self.fc3 = nn.Linear(fc2_units, 1)
         self.reset_parameters()
@@ -69,7 +77,11 @@ class Critic(nn.Module):
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
+        if state.dim() == 1:
+            state = torch.unsqueeze(state, 0)
         xs = F.relu(self.fcs1(state))
+        # applying a batch Normalization on the first layer output
+        xs = self.bn1(xs)
         x = torch.cat((xs, action), dim=1)
         x = F.relu(self.fc2(x))
         return self.fc3(x)
